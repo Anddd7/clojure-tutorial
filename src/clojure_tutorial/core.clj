2a75d45bdab6a3f9872bc98cb3bb668b1d2b1c31
Andong Liao <adliao@thoughtworks.com>
2022-01-18 16:50:37 +0800

    a sample function to parse string and concat string

--- src/clojure_tutorial/core.clj
+++ src/clojure_tutorial/core.clj
@@ -5,8 +5,28 @@
   [x]
   (println x "Hello, World!"))
 
+
 ; main function
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (prn "Hello, World!"))
+
+
+; define set with items
+(def vowel? (set "aeiou"))
+
+; define function [input]
+(defn pig-latin [word]
+  ; define variable = first(word)
+  (let [first-letter (first word)]
+    ; if set.contains
+    (if (vowel? first-letter)
+      ; then str = word + "ay"
+      (str word "ay")
+      ; else str = word[1:] + word[0] + "ay"
+      (str (subs word 1) first-letter "ay"))))
+
+; run it along with this script
+(println (pig-latin "red"))
+(println (pig-latin "orange"))
